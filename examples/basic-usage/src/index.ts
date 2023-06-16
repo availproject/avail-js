d3dd3b42649eed458cdc380bfe768025839a383d
Ghali El Ouarzazi <ghalielouarzazi@hotmail.com>
2023-06-16 19:28:02 +0200

    fix build for optimized npm package, add example, improve tsconfig fo d.ts files

--- examples/basic-usage/src/index.ts
+++ examples/basic-usage/src/index.ts
@@ -1,1 +1,11 @@
-import { initialize } from 'avail-js-sdk/src/chain/index'
+import { initialize } from "avail-js-sdk"
+import { isConnected, disconnect } from 'avail-js-sdk/chain'
+
+const main = async () => {
+    const api = await initialize()
+    const chain = await api.rpc.system.chain()
+    console.log(`Chain: ${chain} - Is connected: ${isConnected()}`)
+    await disconnect()
+    process.exit(0)
+}
+main()
